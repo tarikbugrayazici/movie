c4260e45be679dcd61889ed965e47b497c778982
tarikbugrayazici <tarikbyazici61@gmail.com>
2020-02-22 14:21:22 +0300

    ActorsPhotoEntity converted to Kotlin from Java

---
+++ app/src/main/java/com/example/movies/data/entity/ActorsPhotoEntity.kt
@@ -0,0 +1,9 @@
+package com.example.movies.data.entity
+
+import java.util.ArrayList
+
+class ActorsPhotoEntity {
+    var profiles: ArrayList<ActorsPhotos>? = null
+        internal set
+    val id: Int = 0
+}

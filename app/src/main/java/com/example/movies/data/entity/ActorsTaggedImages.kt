5369691e8579fb7c4b5bd1f2edb91c5cf7a63c0f
tarikbugrayazici <tarikbyazici61@gmail.com>
2020-02-22 14:25:10 +0300

    ActorsTaggedImages converted to Kotlin from Java

---
+++ app/src/main/java/com/example/movies/data/entity/ActorsTaggedImages.kt
@@ -0,0 +1,11 @@
+package com.example.movies.data.entity
+
+import java.util.ArrayList
+
+class ActorsTaggedImages {
+    val id: Int = 0
+    val page: Int = 0
+    val total_results: Int = 0
+    val results: ArrayList<ActorsResult>? = null
+    val total_pages: Int = 0
+}

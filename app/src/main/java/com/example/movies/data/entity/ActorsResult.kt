255f5b581feaa9bb94a5b2b9a39633501bccae50
tarikbugrayazici <tarikbyazici61@gmail.com>
2020-02-22 14:23:54 +0300

    ActorsResult converted to Kotlin from Java

---
+++ app/src/main/java/com/example/movies/data/entity/ActorsResult.kt
@@ -0,0 +1,16 @@
+package com.example.movies.data.entity
+
+
+class ActorsResult {
+    val aspect_ratio: Number? = null
+    val file_path: String? = null
+    val height: Int = 0
+    val iso_639_1: String? = null
+    val vote_average: Number? = null
+    val vote_count: Int = 0
+    val widht: Int = 0
+    val image_type: String? = null
+    val media_type: String? = null
+    val popularity: Number? = null
+    val media: Media? = null
+}

073bec29d1c76b23244ca38a445ed56077e111b3
tarikbugrayazici <tarikbyazici61@gmail.com>
2020-02-22 15:24:55 +0300

    ProductionCompanies converted to Kotlin from Java

--- app/src/main/java/com/example/movies/data/entity/ProductionCompanies.kt
+++ app/src/main/java/com/example/movies/data/entity/ProductionCompanies.kt
@@ -1,8 +1,6 @@
 package com.example.movies.data.entity
 
-class ProductionCompanies {
-    val id: Int = 0
-    val logo_path: String? = null
-    val name: String? = null
-    val origin_country: String? = null
-}
+data class ProductionCompanies(val id: Int? = null,
+                               val logo_path: String? = null,
+                               val name: String? = null,
+                               val origin_country: String? = null)

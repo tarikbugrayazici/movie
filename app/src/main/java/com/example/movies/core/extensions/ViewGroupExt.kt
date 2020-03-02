7f9127d931aeb21590c67bc25b36a7835ee53157
tarikbugrayazici <tarikbyazici61@gmail.com>
2020-03-02 13:55:29 +0300

    no message

--- app/src/main/java/com/example/movies/core/extensions/ViewGroupExt.kt
+++ app/src/main/java/com/example/movies/core/extensions/ViewGroupExt.kt
@@ -4,10 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-/*fun <T : ViewDataBinding> ViewGroup.bind(layoutId: Int): T {
-    return DataBindingUtil.inflate(layoutId, this, true)
-}*/
-
 fun ViewGroup.inflate(layoutId: Int): View {
     return LayoutInflater.from(context).inflate(layoutId, this, false)
 }
--- app/src/main/java/com/example/movies/core/extensions/contextExt.kt
+++ app/src/main/java/com/example/movies/core/extensions/contextExt.kt
@@ -13,19 +13,3 @@ import com.example.movies.ui.actorsdetailtabs.adapter.InfoAdapter
 
 fun Context.showToast(text: CharSequence, duration: Int = Toast.LENGTH_LONG) =
         this?.let { Toast.makeText(this, text, duration).show() }
-
-/*fun InfoAdapter.InfoViewHolder?.inflate(context: Context, layoutId: Int, attachToRoot: Boolean, viewGroup: ViewGroup): RecyclerView.ViewHolder? =
-        LayoutInflater.from(context).inflate(layoutId, viewGroup, attachToRoot)*/
-
-/*fun <T : RecyclerView.ViewHolder> ViewGroup.bind(layoutId: Int): T {
-    return DataBindingUtil.inflate(getLayoutInflater(), layoutId, this, true)
-}
-fun <T : ViewDataBinding> View.bind() = DataBindingUtil.bind<T>(this) as T
-
-fun <T> Collection<T>.ab(result: Result<T>) {
-    val anyResult: Result<T> = result as Result<T>
-
-}
-
-
-val movieresult: Result<MovieCrew> = result as Result<MovieCrew>*/
